a8c166c9d68e870db51bc31b2d7fbef58e97400d
shortf15e <73486744+shortf15e@users.noreply.github.com>
2020-12-08 11:08:31 -0800

    endpoint mapping

---
+++ src/main/java/com/example/demo/EndpointsController.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EndpointsController {
+
+    @GetMapping("/")
+    public String getIndex() {
+        return "GET to index route";
+    }
+
+    @GetMapping("/tasks")
+    public String getTasks () {
+        return "These are the tasks";
+    }
+
+    @PostMapping("/tasks")
+    public String createTask() {
+        return "You just POSTed to /tasks";
+    }
+}
---
+++ src/test/java/com/example/demo/PagesControllerTest.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(PagesController.class)
+
+public class PagesControllerTest {
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testHomepage() throws Exception {
+
+        this.mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("Hello World!"));
+
+    }
+}
