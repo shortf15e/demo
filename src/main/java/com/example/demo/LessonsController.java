e5809fa0ca860f8ede22e1f132dd72fb7d69d1cd
shortf15e <73486744+shortf15e@users.noreply.github.com>
2020-12-10 22:49:42 -0800

    Patch working for Lessons

--- src/main/java/com/example/demo/LessonsController.java
+++ src/main/java/com/example/demo/LessonsController.java
@@ -1,7 +1,9 @@
 package com.example.demo;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.*;
 import java.util.HashMap;
 import java.util.List;
 
@@ -22,6 +24,7 @@ public class LessonsController {
 
     @PostMapping("")
     public Lesson create(@RequestBody Lesson lesson) {
+
         return this.repository.save(lesson);
     }
 
@@ -33,6 +36,23 @@ public class LessonsController {
     @DeleteMapping("/{id}")
     public void deleteLessonById (@PathVariable Long id) {
         repository.deleteById(id);
+    }
+
+    @PatchMapping("/{id}")
+    public void updateLessonById(@PathVariable Long id, @RequestBody Lesson lesson) {
+        Lesson tempLesson  = repository.findById(id).get();
+        if (!repository.findById(id).equals(lesson)) {
+            if (!tempLesson.getDeliveredOn().equals(lesson.getDeliveredOn())){
+                tempLesson.setDeliveredOn(lesson.getDeliveredOn());
+            }
+            if (!tempLesson.getId().equals(lesson.getId())) {
+                tempLesson.setId(lesson.getId());
+            }
+            if (!tempLesson.getTitle().equals(lesson.getTitle())) {
+                tempLesson.setTitle(lesson.getTitle());
+            }
+            this.repository.save(tempLesson);
+        }
     }
 
 }
--- src/test/java/com/example/demo/LessonsControllerTest.java
+++ src/test/java/com/example/demo/LessonsControllerTest.java
@@ -7,14 +7,17 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import javax.transaction.Transactional;
 
 import java.util.Date;
 
-import static org.hamcrest.Matchers.equalTo;
+
+import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.instanceOf;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -50,6 +53,49 @@ public class LessonsControllerTest {
         this.mvc.perform(request)
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id", instanceOf(Number.class)));
+    }
+
+    @Test
+    @Transactional
+    public void patchGetLessonByID() throws Exception {
+        Lesson lesson = new Lesson();
+        lesson.setId(1L);
+        lesson.setDeliveredOn(new Date());
+        lesson.setTitle("Test Title");
+        repository.save(lesson);
+
+        lesson.setId(2L);
+        lesson.setDeliveredOn(new Date());
+        lesson.setTitle("Test Title 2");
+        repository.save(lesson);
+
+        lesson.setId(3L);
+        lesson.setDeliveredOn(new Date());
+        lesson.setTitle("Test Title 3");
+        repository.save(lesson);
+
+        lesson.setId(4L);
+        lesson.setDeliveredOn(new Date());
+        lesson.setTitle("Test Title 4");
+        repository.save(lesson);
+
+        lesson.setDeliveredOn(new Date());
+        lesson.setTitle("I am you're mommy!");
+        repository.save(lesson);
+
+        MockHttpServletRequestBuilder request = patch("/lessons/" + 3L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\n" +
+                        "    \"id\": 3,\n" +
+                        "    \"title\": \"Who is you're daddy?\",\n" +
+                        "    \"deliveredOn\": \"2012-03-14\"\n" +
+                        "}");
+        this.mvc.perform(request)
+                .andExpect(status().isOk())
+                .andReturn();
+        String actual = repository.findById(3L).get().getTitle();
+        String expected = "Who is you're daddy?";
+        assertEquals(expected, actual);
     }
 
 
